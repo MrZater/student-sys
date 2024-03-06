0d574313f69ff29d7bb70fb733321935eeee0fde
zt <zhoutao@ydmob.com>
2024-03-06 18:54:49 +0800

    class2-1

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
  * @Author: zt zhoutao@ydmob.com
  * @Date: 2024-02-05 18:17:39
  * @LastEditors: zt zhoutao@ydmob.com
- * @LastEditTime: 2024-02-07 11:04:05
+ * @LastEditTime: 2024-03-06 18:25:56
  * @FilePath: /student-sys/README.md
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%A
 -->
@@ -14,6 +14,7 @@
 ## 使用第三方库
 
 1. express
+   [express官网](https://www.expressjs.com.cn)
 2. koa2
 
 一个完整的系统，最重要的验证，一定是在服务器端
@@ -21,3 +22,12 @@
 1. 客户端（浏览器、app、pad、小程序）：用户体验
 2. 服务器端逻辑验证（业务逻辑层的验证）：为了业务逻辑性的安全性、完整性
 3. 数据库验证：为了保证数据完整性
+
+
+## REST风格的API接口
+
+1. /api/student post 添加
+2. /api/student get 获取
+3. /api/student put 修改
+3. /api/student delete 删除
+
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,26 @@
-import StudentService from "./services/StudentService"
+/*
+ * @Author: zt zhoutao@ydmob.com
+ * @Date: 2024-02-05 18:56:43
+ * @LastEditors: zt zhoutao@ydmob.com
+ * @LastEditTime: 2024-03-06 18:29:18
+ * @FilePath: /student-sys/src/index.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import express, { Express, Request, Response } from 'express'
+const app: Express = express()
+const port = 3000
+app.listen(port, () => {
+    console.log(`server is running at http://localhost:${port}`)
+})
 
-require('./init')
+app.get('/test', (request: Request, response: Response) => {
+    // response.setHeader('hahahha', '111')
+    // response.send('Hello World!')
+    // response.status(302).header('location', 'http://www.baidu.com').end()
+    response.status(302).location('http://www.baidu.com').end()
+})
 
-StudentService.getStudents().then(r=> {
-    console.log(r);
+app.get('/test/:id', (request: Request, response: Response) => {
+    console.log(request.path, request.headers, request.query, request.params);
+    response.send('Hello World!' + request.params.id)
 })
