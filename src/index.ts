b8f1b08cd58354a70f5aba2564ba3f7fd6b4067e
zt <zhoutao@ydmob.com>
2024-04-09 18:54:51 +0800

    class2-2

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,9 @@
  * @Author: zt zhoutao@ydmob.com
  * @Date: 2024-02-05 18:56:43
  * @LastEditors: zt zhoutao@ydmob.com
- * @LastEditTime: 2024-03-06 19:07:21
+ * @LastEditTime: 2024-04-09 18:51:00
  * @FilePath: /student-sys/src/index.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 require('./init')
+console.log(process.env.NODE_ENV)
