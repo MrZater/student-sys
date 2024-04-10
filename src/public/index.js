58dfe62b6849669444bfcf0ad7bcf6a8895719a7
zt <zhoutao@ydmob.com>
2024-04-10 15:03:24 +0800

    calss2-4

---
+++ src/public/index.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: zt zhoutao@ydmob.com
+ * @Date: 2024-04-10 12:04:20
+ * @LastEditors: zt zhoutao@ydmob.com
+ * @LastEditTime: 2024-04-10 12:04:26
+ * @FilePath: /student-sys/src/public/index.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+console.log('test')
--- src/routes/init.ts
+++ src/routes/init.ts
@@ -2,31 +2,33 @@
  * @Author: zt zhoutao@ydmob.com
  * @Date: 2024-03-06 19:06:24
  * @LastEditors: zt zhoutao@ydmob.com
- * @LastEditTime: 2024-04-10 11:52:12
+ * @LastEditTime: 2024-04-10 14:57:42
  * @FilePath: /student-sys/src/routes/init.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-import express, { Express, Request, Response, NextFunction, IRouterMatcher } from 'express'
+import express, { Express, static as static_, urlencoded, json } from 'express'
 import errorMiddleware from './errorMiddleware'
-import staticMiddleware from './staticMiddleware'
+import path from 'path'
 const app: Express = express()
-// 错误处理中间件,静态资源中间件
-app.use(errorMiddleware, staticMiddleware)
+// 错误处理中间件
+app.use(errorMiddleware)
+// 静态资源中间件
+app.use('/static',static_(path.resolve(__dirname, '../public')))
+// 请求体解析中间件
+app.use(urlencoded({
+    extended: true
+}))
+// JSON解析中间件
+app.use(json())
 const port = 3000
 app.listen(port, () => {
     console.log(`server is running at http://localhost:${port}`)
 })
-
-app.get('/test',
-    (req: Request, resp: Response, next: NextFunction) => {
-        console.log('handler1');
-        resp.status(200)
-        // resp.end()
-        // next()
-        throw new Error('报错')
-    },
-    (req: Request, resp: Response) => {
-        console.log('handler2');
-        resp.send()
-    }
-)
+app.post('/test', (req, res, next) => {
+    console.log(req.body)
+    res.send('test')
+})
+app.get('/test', (req, res, next) => {
+    console.log(req.body)
+    res.send('test')
+})
