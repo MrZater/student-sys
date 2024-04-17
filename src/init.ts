16a231514984303e5d384642c8f752ec37d2e910
zt <zhoutao@ydmob.com>
2024-04-17 14:23:29 +0800

    feat: 特性: 🚀 class2-9

--- src/init.ts
+++ src/init.ts
@@ -2,18 +2,10 @@
  * @Author: zt zhoutao@ydmob.com
  * @Date: 2024-03-01 10:54:07
  * @LastEditors: zt zhoutao@ydmob.com
- * @LastEditTime: 2024-04-15 18:15:25
+ * @LastEditTime: 2024-04-15 19:43:09
  * @FilePath: /student-sys/src/init.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 require('./models/init')
 require('./services/init')
 require('./routes/init')
-
-
-// import Crypt from "./util/crypt";
-
-// const crypt = new Crypt();
-
-// const a = crypt.encrypt("123456");
-// console.log(a, crypt.decrypt(a))
--- src/util/crypt.ts
+++ src/util/crypt.ts
@@ -1,3 +1,4 @@
+// token加密
 // const secret = Math.random().toString(36).slice(-8) + Math.random().toString(36).slice(-8);
 import crypto from 'crypto'
 
@@ -6,22 +7,24 @@ export default class Crypt {
         this.secret = Buffer.from('zvis0bbaftlok3pt')
         this.iv = Buffer.from('zvis0bbaftlok3pt')
     }
+    // 密钥
     public secret: Buffer
+    // iv
     public iv: Buffer
+    // 加密
     public encrypt(str: string) {
         const cry = crypto.createCipheriv('aes-128-cbc', this.secret, this.iv)
+        // utf-8 ====> 二进制
         let result = cry.update(str, 'utf-8', 'hex')
         result += cry.final('hex')
         return result
     }
-
+    // 解密
     public decrypt(signed: string) {
         const cry = crypto.createDecipheriv('aes-128-cbc', this.secret, this.iv)
+        // 二进制 ====> utf-8
         let result = cry.update(signed, 'hex', 'utf-8')
         result += cry.final('utf-8')
         return result
     }
 }
-
-const a = new Crypt().encrypt('123')
-console.log(a)
