5b83b7ddce2bb5dafa650c9db5a65d8a9b856f47
zt <zhoutao@ydmob.com>
2024-04-29 19:40:29 +0800

    feat: 特性: 🚀 class2-11

--- src/public/index.js
+++ src/public/index.js
@@ -2,7 +2,7 @@
  * @Author: zt zhoutao@ydmob.com
  * @Date: 2024-04-10 12:04:20
  * @LastEditors: zt zhoutao@ydmob.com
- * @LastEditTime: 2024-04-29 19:27:35
+ * @LastEditTime: 2024-04-29 19:33:31
  * @FilePath: /student-sys/src/public/index.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -14,7 +14,8 @@ fetch('http://localhost:3000/api/student', {
     headers: {
         'content-type': 'application/json',
         'a': '123'
-    }
+    },
+    credentials: 'include'
 }).then(res => {
     console.log(res)
 })
--- src/routes/init.ts
+++ src/routes/init.ts
@@ -2,7 +2,7 @@
  * @Author: zt zhoutao@ydmob.com
  * @Date: 2024-03-06 19:06:24
  * @LastEditors: zt zhoutao@ydmob.com
- * @LastEditTime: 2024-04-29 19:28:12
+ * @LastEditTime: 2024-04-29 19:38:48
  * @FilePath: /student-sys/src/routes/init.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -23,7 +23,17 @@ app.use(errorMiddleware)
 // 静态资源中间件
 app.use('/', static_(path.resolve(__dirname, '../public')))
 // 跨域中间件
-app.use(corsMiddleware())
+const whiteList = ['null']
+app.use(corsMiddleware({
+    origin(origin, callback) {
+        if (origin && whiteList.includes(origin)) {
+            callback(null, origin)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    },
+    credentials: true
+}))
 // 加入cookie-parser中间件
 // 加入之后，会在req对象中添加cookies属性，用于处理所有请求传递过来的cookie
 // 加入之后，会在res对象中注入cookie方法，用于设置cookie
